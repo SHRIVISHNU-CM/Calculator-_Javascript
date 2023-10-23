ac002b26476288278234a940917585fc737fd7e4
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2023-10-23 22:37:19 +0530

    Calculator

---
+++ app.js
@@ -0,0 +1,23 @@
+let area = document.getElementById('text_area');
+let btn = document.querySelectorAll('button');
+let string = " "
+btn.forEach((Element)=>{
+    Element.addEventListener('click',(e)=>{
+        if(e.target.innerText == "="){
+            string= String(eval(string))
+            area.value =string;
+        }
+        else if(e.target.innerText == 'C'){
+            string= "";
+            area.value = string;
+        }
+        else if(e.target.innerText == 'DE'){
+           string = string.substring(0,string.length-1)
+           area.value = string 
+        }
+        else{
+            string += e.target.innerText;
+            area.value = string
+        }
+    })
+})
